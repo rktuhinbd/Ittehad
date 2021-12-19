0046de74e53c468efca41a5ddb75f17f406bbd50
Md Rejaul Karim <tuhin@10minuteschool.com>
2021-12-19 17:15:39 +0600

    Splash Screen implemented, assets imported

--- app/src/main/java/com/madaninagar/madani/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/madaninagar/madani/splash/SplashScreenActivity.kt
@@ -1,12 +1,28 @@
 package com.madaninagar.madani.splash
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.madaninagar.madani.R
+import com.madaninagar.madani.databinding.ActivitySplashScreenBinding
+import com.madaninagar.madani.view.activity.HomeActivity
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler().postDelayed({ //This method will be executed once the timer is over
+            // Start your app main activity
+            val i = Intent(this@SplashScreenActivity, HomeActivity::class.java)
+            startActivity(i)
+            // close this activity
+            finish()
+        }, 3000)
     }
 }
