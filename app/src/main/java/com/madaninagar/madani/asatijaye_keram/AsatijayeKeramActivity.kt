805af56ef5e23a2fc0515ab2540f86a5bf316018
Md. Rejaul Karim <rktuhinbd@gmail.com>
2021-12-20 14:31:23 +0600

    Searchable toolbar created in AsatijayeKeramActivity

--- app/src/main/java/com/madaninagar/madani/asatijaye_keram/AsatijayeKeramActivity.kt
+++ app/src/main/java/com/madaninagar/madani/asatijaye_keram/AsatijayeKeramActivity.kt
@@ -1,6 +1,7 @@
 package com.madaninagar.madani.asatijaye_keram
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.madaninagar.madani.databinding.ActivityAsatijayeKeramBinding
 
@@ -12,5 +13,16 @@ class AsatijayeKeramActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAsatijayeKeramBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.toolbar.ivSearch.setOnClickListener {
+            binding.toolbar.layoutTitle.visibility = View.GONE
+            binding.toolbar.layoutSearch.visibility = View.VISIBLE
+        }
+
+        binding.toolbar.ivCancel.setOnClickListener {
+            binding.toolbar.etSearch.text?.clear()
+            binding.toolbar.layoutSearch.visibility = View.GONE
+            binding.toolbar.layoutTitle.visibility = View.VISIBLE
+        }
     }
 }
